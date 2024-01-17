81cf7757b2babafc8a505392af7a90e16178056b
Retrovyrus <mauricio.mas2009@gmail.com>
2024-01-17 09:47:18 -0300

    dev 5-2

---
+++ JavaScript_1/Desenvolvimento_5_2/Calculadora.js
@@ -0,0 +1,21 @@
+
+function Calculadora(num1,num2){
+  
+let soma = `A soma de ${num1} e ${num2} é ${num1 + num2}`;  
+let subtracao = `A diferença de ${num1} e ${num2} é ${num1 - num2}`;
+let multiplicacao = `A multiplicação de ${num1} e ${num2} é ${num1 * num2}`;
+let divisao = `A divisão de ${num1} e ${num2} é ${num1 / num2}`;
+
+console.log(soma); 
+console.log(subtracao); 
+console.log(multiplicacao); 
+console.log(divisao); 
+
+  
+}
+
+
+
+Calculadora(4,5);
+
+
